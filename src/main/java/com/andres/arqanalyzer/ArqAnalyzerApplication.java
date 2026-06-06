6c123e4d4a2445b0b52878aa111ea77ab5eb5c49
Felipe Andres <felipevsconcelos@gmail.com>
2026-06-06 09:19:25 -0300

    fix: remove unused ServletWebServerApplicationContext import

--- src/main/java/com/andres/arqanalyzer/ArqAnalyzerApplication.java
+++ src/main/java/com/andres/arqanalyzer/ArqAnalyzerApplication.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.nio.file.Path;
